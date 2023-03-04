e648279950eff64adc2e25d45b18d77b992f9411
Edjancarlos <edjan.nogueira14@gmail.com>
2023-03-04 14:48:28 -0300

    feat: adiciona logo

--- src/components/Template/Sidebar.tsx
+++ src/components/Template/Sidebar.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import Logo from "../../assets/logo.png";
+
 export function Sidebar() {
-  return <div className="h-screen w-72 bg-white">teste</div>;
+  return (
+    <div className="flex flex-col h-screen w-72 bg-white">
+      <div
+        id="logo"
+        className="pl-[35px] pt-[32px] pr-[80px] pb-[37px] border-b-2 border-[#F4F7FE]"
+      >
+        <Image
+          src={Logo}
+          alt="logo"
+          className="max-w-[175px] max-h-[61px] w-auto h-auto"
+        />
+      </div>
+    </div>
+  );
 }
